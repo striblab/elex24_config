95c94a1b177f114124b46d56da5a8c1190116d46
Jeffrey Hargarten <harga012@umn.edu>
2024-05-25 17:03:02 -0500

    filelist

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,12 @@
   let isolate = '0';
   let interactive = '1';
   let shading = '0';
+  let height = '600';
   let hed = '';
   let chatter = '';
-  let showText = '0';
+  let showtext = '0';
   let clicky = '1';
   let search = '1';
-  let height = '600';
   let elections = []; // This will hold the fetched data
 
   onMount(async () => {
@@ -31,12 +31,12 @@
       filter: isolate,
       interactive,
       shading,
-      search,
-      clicky,
-      height,
-      text: showText,
       title: hed,
-      chatter
+      chatter,
+      height,
+      text: showtext,
+      clicky,
+      search
     }).toString();
 
     let tag = `${baseUrl}?${params}`;
@@ -53,11 +53,10 @@
     <h1>StribLab Election 2022 Precinct Mapper</h1>
     <form id="form_id" class="appnitro" on:submit={handleSubmit}>
       <div class="form_description">
-        <h2>Configure and generate precinct result maps</h2>
-        <p>Embeddable Mapbox visualizations.</p>
+        <h2>StribLab Election 2022 Precinct Mapper</h2>
+        <p>Configure and generate precinct result maps.</p>
       </div>
 
-      <!-- Dropdown for selecting contest -->
       <div class="element">
         <label for="eOffice">Select Office:</label>
         <select bind:value={district} class="element select medium" id="eOffice">
@@ -68,131 +67,227 @@
         </select>
       </div>
 
-      <!-- Radio buttons for district borders -->
-      <div class="element">
-        <label>Boundaries:</label>
-        <div class="options">
-          <label class="control control-radio">
-            <input type="radio" id="boundaryEnable" bind:group={boundaries} value="1" checked>
-            <div class="control_indicator"></div>
-            Enable
-          </label>
-          <label class="control control-radio">
-            <input type="radio" id="boundaryDisable" bind:group={boundaries} value="0">
-            <div class="control_indicator"></div>
-            Disable
-          </label>
-        </div>
-      </div>
-
-      <!-- Radio buttons for zoom mode -->
-      <div class="element">
-        <label>Zoom Mode:</label>
-        <div class="options">
-          <label class="control control-radio">
-            <input type="radio" id="zoomState" bind:group={isolate} value="0" checked>
-            <div class="control_indicator"></div>
-            State
-          </label>
-          <label class="control control-radio">
-            <input type="radio" id="zoomMetro" bind:group={isolate} value="1">
-            <div class="control_indicator"></div>
-            Metro
-          </label>
-        </div>
-      </div>
-
-      <!-- Radio buttons for interactive controls -->
-      <div class="element">
-        <label>Interactivity</label>
-        <div class="options">
-          <label class="control control-radio">
-            <input type="radio" id="interactiveYes" bind:group={interactive} value="1" checked>
-            <div class="control_indicator"></div>
-            Enable
-          </label>
-          <label class="control control-radio">
-            <input type="radio" id="interactiveNo" bind:group={interactive} value="0">
-            <div class="control_indicator"></div>
-            Disable
-          </label>
-        </div>
-      </div>
-
-      <!-- Radio buttons for map click interactivity -->
-      <div class="element">
-        <label>Controls and Tooltips:</label>
-        <div class="options">
-          <label class="control control-radio">
-            <input type="radio" id="clickEnable" bind:group={clicky} value="1" checked>
-            <div class="control_indicator"></div>
-            Enable
-          </label>
-          <label class="control control-radio">
-            <input type="radio" id="clickDisable" bind:group={clicky} value="0">
-            <div class="control_indicator"></div>
-            Disable
-          </label>
-        </div>
-      </div>
-
-      <!-- Radio buttons for map search -->
-      <div class="element">
-        <label>Search Feature:</label>
-        <div class="options">
-          <label class="control control-radio">
-            <input type="radio" id="searchEnable" bind:group={search} value="1" checked>
-            <div class="control_indicator"></div>
-            Enable
-          </label>
-          <label class="control control-radio">
-            <input type="radio" id="searchDisable" bind:group={search} value="0">
-            <div class="control_indicator"></div>
-            Disable
-          </label>
-        </div>
-      </div>
-
-      <div class="element">
-        <label>Header Text:</label>
-          <div class="options">
+      <ul>
+      <li>
+        <label class="description" for="eDistricts">District borders</label>
+        <span>
             <label class="control control-radio">
-              <input type="radio" id="headerYes" bind:group={showText} value="1" >
-              <div class="control_indicator"></div>
-              Enable
+                <input id="eDistricts_1" name="eDistricts" class="element radio" type="radio" value="1" checked bind:group={boundaries}  />
+                <div class="control_indicator"></div>
+                Enable
             </label>
             <label class="control control-radio">
-              <input type="radio" id="headerNo" bind:group={showText} value="0" checked>
-              <div class="control_indicator"></div>
-              Disable
+                <input id="eDistricts_2" name="eDistricts" class="element radio" type="radio" value="0" bind:group={boundaries} />
+                <div class="control_indicator"></div>
+                Disable
             </label>
-          </div>
-      </div>
-
-
-      <!-- Text inputs for data visualization headings and chatter -->
-      <div class="element">
-        <label for="eHed">Map Title:</label>
-      </div>
-      <input type="text" bind:value={hed} class="element text medium" maxlength="255" id="eHed">
-      <div class="element">
-        <label for="eChatter">Description:</label>
-        <textarea bind:value={chatter} class="element textarea medium" id="eChatter"></textarea>
-      </div>
-
-      <!-- Text input for map height -->
-      <div class="element">
-        <label for="eHeight">Map Height:</label>
-      </div>
-      <input type="text" bind:value={height} class="element text medium" maxlength="255" id="eHeight">
-
-      <!-- Submission button -->
-      <button type="submit" class="button_text">GENERATE</button>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eIsolate">Zoom mode</label>
+        <span>
+            <label class="control control-radio">
+                <input id="eIsolate_1" name="eIsolate" class="element radio" type="radio" value="0" checked bind:group={isolate} />
+                <div class="control_indicator"></div>
+                State
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_2" name="eIsolate" class="element radio" type="radio" value="1" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Metro
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_3" name="eIsolate" class="element radio" type="radio" value="2" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Isolate districts
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_4" name="eIsolate" class="element radio" type="radio" value="3" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Large map
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_5" name="eIsolate" class="element radio" type="radio" value="4" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Hennepin County
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_6" name="eIsolate" class="element radio" type="radio" value="5" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Duluth
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_7" name="eIsolate" class="element radio" type="radio" value="6" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                St. Cloud
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_8" name="eIsolate" class="element radio" type="radio" value="7" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Moorhead
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_9" name="eIsolate" class="element radio" type="radio" value="8" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Mankato
+            </label>
+            <label class="control control-radio">
+                <input id="eIsolate_10" name="eIsolate" class="element radio" type="radio" value="9" bind:group={isolate} />
+                <div class="control_indicator"></div>
+                Rochester
+            </label>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eInteract">State/metro controls</label>
+        <span>
+            <label class="control control-radio">
+                <input id="eInteract_1" name="eInteract" class="element radio" type="radio" value="1" checked bind:group={interactive} />
+                <div class="control_indicator"></div>
+                Enable
+            </label>
+            <label class="control control-radio">
+                <input id="eInteract_2" name="eInteract" class="element radio" type="radio" value="0" bind:group={interactive} />
+                <div class="control_indicator"></div>
+                Disable
+            </label>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eClicky">Map interactivity</label>
+        <span>
+            <label class="control control-radio">
+                <input id="eClicky_1" name="eClicky" class="element radio" type="radio" value="1" checked bind:group={clicky} />
+                <div class="control_indicator"></div>
+                Enable
+            </label>
+            <label class="control control-radio">
+                <input id="eClicky_2" name="eClicky" class="element radio" type="radio" value="0" bind:group={clicky}  />
+                <div class="control_indicator"></div>
+                Disable
+            </label>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eSearch">Map search</label>
+        <span>
+            <label class="control control-radio">
+                <input id="eSearch_1" name="eSearch" class="element radio" type="radio" value="1" checked bind:group={search} />
+                <div class="control_indicator"></div>
+                Enable
+            </label>
+            <label class="control control-radio">
+                <input id="eSearch_2" name="eSearch" class="element radio" type="radio" value="0" bind:group={search} />
+                <div class="control_indicator"></div>
+                Disable
+            </label>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eShading">Result shading mode</label>
+        <span>
+            <label class="control control-radio">
+                <input id="eShading_1" name="eShading" class="element radio" type="radio" value="0" checked bind:group={shading} />
+                <div class="control_indicator"></div>
+                Partisan margin + vote density
+            </label>
+            <label class="control control-radio">
+                <input id="eShading_2" name="eShading" class="element radio" type="radio" value="1" bind:group={shading} />
+                <div class="control_indicator"></div>
+                Classic partisan margin ramp
+            </label>
+            <label class="control control-radio">
+                <input id="eShading_3" name="eShading" class="element radio" type="radio" value="2" bind:group={shading} />
+                <div class="control_indicator"></div>
+                Partisan binary [Red vs. Blue]
+            </label>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eHeader">Show header text</label>
+        <span>
+            <label class="control control-radio">
+                <input id="eText_1" name="eText" class="element radio" type="radio" value="1" bind:group={showtext} />
+                <div class="control_indicator"></div>
+                Enable
+            </label>
+            <label class="control control-radio">
+                <input id="eText_2" name="eText" class="element radio" type="radio" value="0" checked bind:group={showtext}  />
+                <div class="control_indicator"></div>
+                Disable
+            </label>
+        </span> 
+    </li>
+    <li>
+        <label class="description" for="eHed">Dataviz hed text</label>
+        <div>
+            <input id="eHed" name="eHed" class="element text medium" type="text" maxlength="255" bind:value={hed} /> 
+        </div> 
+    </li>
+    <li>
+        <label class="description" for="eChatter">Dataviz chatter text</label>
+        <div>
+            <textarea id="eChatter" name="eChatter" class="element textarea medium" bind:value={chatter}></textarea> 
+        </div> 
+    </li>
+    <li>
+        <label class="description" for="eHeight">Map height in px</label>
+        <div>
+            <input id="eHeight" name="eHeight" class="element text medium" type="text" maxlength="255" bind:value={height} /> 
+        </div> 
+    </li>
+    <li class="buttons">
+        <input type="hidden" name="form_id" value="42838" />
+        <button type="submit" class="button_text">GENERATE</button>
+    </li>
+  </ul>
     </form>
-  </div>
+
+<hr />
+<div id="downloads">
+<h2>Results file downloads</h2>
+<p class="dlabel">Presidential</p>
+<ul>
+    <li><a href="../store/potus-results-geo.json">GEOJSON: POTUS results by precinct</a></li>
+    <li><a href="../store/potus-results-county.json">GEOJSON: POTUS results by county</a></li>
+    <li><a href="../store/potus-results-district.json">GEOJSON: POTUS results by congressional district</a></li>
+    <li><a href="../store/potus-results-leg.json">GEOJSON: POTUS results by legislative district</a></li>
+</ul>
+
+<p class="dlabel">Senatorial</p>
+<ul>
+    <li><a href="../store/senate-results-geo.json">GEOJSON: Senate results by precinct</a></li>
+    <li><a href="../store/senate-results-district.json">GEOJSON: Senate results by congressional district</a></li>
+</ul>
+
+
+<p class="dlabel">Congressional</p>
+<ul>
+    <li><a href="../store/congress-results-geo.json">GEOJSON: U.S. House results by precinct</a></li>
+    <li><a href="../store/congress-results-district.json">GEOJSON: U.S. House results by congressional district</a></li>
+</ul>
+
+
+<p class="dlabel">Legislative</p>
+<ul>
+    <li><a href="../store/mnleg-results-geo.json">GEOJSON: MNLEG results by precinct</a></li>
+    <li><a href="../store/mnleg-results-leg.json">GEOJSON: MNLEG by legislative district</a></li>
+</ul>
+</div>
 </main>
 
+
+
+
 <style>
+.dlabel {
+    margin-top:18px;
+    font-weight:bold
+}
+#downloads li {
+    font-family: Arial, Helvetica, sans-serif;
+}
+
   #form_container {
     max-width: 600px; /* Sets a max width for the form container */
     margin: 0 auto; /* Centers the form on the page */
@@ -273,8 +368,8 @@
         position: absolute;
         top: 2px;
         left: 0;
-        height: 20px;
-        width: 20px;
+        height: 10px;
+        width: 10px;
         background: #e6e6e6;
         border: 0px solid #000000;
         border-radius: undefinedpx;
@@ -310,8 +405,8 @@
     }
 
     .control-radio .control_indicator:after {
-        left: 7px;
-        top: 7px;
+        left: 2px;
+        top: 2px;
         height: 6px;
         width: 6px;
         border-radius: 50%;
